e70f74777a47e0ca03f52255bdc89daa742bd5f7
ryczko.dawid <ryczko.dawid@gmail.com>
2019-08-17 18:39:17 +0200

    Interpolation

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,18 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
+import { InterpolationComponent } from './interpolation/interpolation.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {
+    path: '',
+    redirectTo: 'interpolation',
+    pathMatch: 'full'
+  },
+  {
+    path: 'interpolation',
+    component: InterpolationComponent
+  }
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
---
+++ src/app/interpolation/interpolation.component.ts
@@ -0,0 +1,30 @@
+import { Component } from "@angular/core";
+
+@Component({
+  selector: "app-interpolation",
+  templateUrl: "./interpolation.component.html",
+  styleUrls: ["./interpolation.component.scss"]
+})
+export class InterpolationComponent {
+  title = "Przykłady interpolacji";
+
+  show = false;
+
+  constructor() {
+   // Dodaję asynchorniczne zdarzenie i zmuszam angulara do uruchamiania Change Detection
+    window.addEventListener("mousemove", () => {});
+  }
+
+  getInfo() {
+    return "Wyświetlone z metody";
+  }
+
+  get info() {
+    return "Wyświetlone przez getter";
+  }
+
+  getNumber() {
+    console.log("Change Detection uruchamia się cały czas");
+    return 2;
+  }
+}
